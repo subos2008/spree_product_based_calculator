5ef55358788ccb06df07561ec43e78e82c2fe371
Ryan Cocks <ryan@ryancocks.net>
2014-02-19 17:37:32 +1300

    Fix up shipping rate Deface on variants page

--- app/overrides/spree/admin/variant_form.rb
+++ app/overrides/spree/admin/variant_form.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(
                       :name => "add shipping_rate field to variant form",
                       :virtual_path => "spree/admin/variants/_form",
-                      :insert_after => "code[erb-loud]:contains('text_field :cost_price')",
+                      :insert_bottom => "[data-hook='admin_variant_form_fields']",
                       :partial => "spree/admin/variants/shipping_rate_field"
                     )

c190328812b2bdf4e1fb0246dd253c3bb749585a
Ryan Cocks <ryan@ryancocks.net>
2014-02-19 17:31:19 +1300

    Fix up shipping rate Deface on product page

--- app/overrides/spree/admin/product_form.rb
+++ app/overrides/spree/admin/product_form.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(
                       :name => "add shipping_rate field to product form",
                       :virtual_path => "spree/admin/products/_form",
-                      :insert_before => "code[erb-loud]:contains('f.field_container :available_on')",
+                      :insert_bottom => "[data-hook='admin_product_form_additional_fields']",
                       :partial => "spree/admin/products/shipping_rate_field"
                     )
